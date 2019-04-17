ba9a0352829d2048ab62645aaeb25eb2b9a52ca0
georgesuarez <georgejsuarez@gmail.com>
2019-04-17 14:29:47 -0700

    Added a health and destroy systems

---
+++ Quiz1/Assets/Scripts/ComponentSystems/DestroySystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+
+public class DestroySystem : ComponentSystem
+{
+    public EntityQuery entityQuery;
+
+    protected override void OnCreateManager()
+    {
+        entityQuery = GetEntityQuery(typeof(Destroy));
+    }
+
+    protected override void OnUpdate()
+    {
+        using (var destroys = entityQuery.ToEntityArray(Allocator.TempJob))
+        {
+            foreach (var destroy in destroys)
+            {
+                EntityManager.DestroyEntity(destroy);
+            }
+        }
+    }
+}
---
+++ Quiz1/Assets/Scripts/JobComponentSystems/HealthSystem.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class HealthSystem : JobComponentSystem
+{
+    BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
+    struct HealthJob : IJobForEachWithEntity<Health>
+    {
+        public EntityCommandBuffer CommandBuffer;
+        public bool rightClick;
+
+        public void Execute(Entity entity, int index, ref Health health)
+        {
+            if (rightClick)
+            {
+                health.Value -= 10;
+            }
+
+            if (health.Value <= 0)
+            {
+                var destroy = new Destroy();
+                CommandBuffer.AddComponent(entity, destroy);
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new HealthJob
+        {
+            CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer(),
+            rightClick = GetSingleton<SingletonMouseInput>().RightClickDown
+
+        }.ScheduleSingle(this, inputDeps);
+
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+
+        return job;
+    }
+}
