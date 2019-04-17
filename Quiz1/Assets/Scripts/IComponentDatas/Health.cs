65669c1dd4ec7af1449cdd52eaeb65a3afe95b88
George Suarez <georgejsuaraez@gmail.com>
2019-04-17 10:52:27 -0700

    Added Health component

---
+++ Quiz1/Assets/Scripts/IComponentDatas/Health.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public struct Health : IComponentData
+{
+    public int Value;
+}
---
+++ Quiz1/Assets/Scripts/Monobehaviours/AddHealth.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class AddHealth : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var health = new Health
+        {
+            Value = 100
+        };
+        dstManager.AddComponentData(entity, health);
+    }
+    
+    
+}
