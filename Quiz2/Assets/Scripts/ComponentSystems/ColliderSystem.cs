cd043359da57f115f851b7ed8d219896e5c734b6
georgesuarez <georgejsuarez@gmail.com>
2019-04-24 16:30:54 -0700

    Changed the meteaor prefab to multiple meteaors

---
+++ Quiz2/Assets/Scripts/ComponentSystems/ColliderSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+
+public class ColliderSystem : ComponentSystem
+{
+    public EntityQuery entityQuery;
+
+    protected override void OnCreateManager()
+    {
+        entityQuery = GetEntityQuery(typeof(Collided));
+    }
+
+    protected override void OnUpdate()
+    {
+        using (var collided = entityQuery.ToEntityArray(Allocator.TempJob))
+        {
+            foreach (var destroy in collided)
+            {
+                EntityManager.DestroyEntity(destroy);
+            }
+        }
+    }
+}
